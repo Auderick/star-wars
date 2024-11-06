85e4cab1a4e884111804ef864688846a5922c734
Christophe Guthoerl <contact@webtransform.fr>
2024-11-06 08:03:59 +0100

    création du controleur home+index.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(HttpClientInterface $httpClientInterface): Response
     {
+        /* Pour envoyer une requête HTTP, il faut utiliser l'interface HttpClientInterface.*/
+        $personnage = $httpClientInterface->request('GET', 'https://swapi.py4e.com/api/people/');
+        
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'personnages' => $personnage->toArray()['results'],
         ]);
     }
 }
